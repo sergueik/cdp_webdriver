52f6cf81aeee9d8a9a5dcfafdba1b069d5cab987
Serguei Kouzmine <kouzmine_serguei@yahoo.com>
2021-03-11 02:41:32 -0500

    intermediate revision - fixd the "Runtime.evaluate" test for css selector and xpathgti

--- src/test/java/example/RuntimeTest.java
+++ src/test/java/example/RuntimeTest.java
@@ -3,7 +3,10 @@ package example;
 import static org.hamcrest.CoreMatchers.is;
 import static org.hamcrest.CoreMatchers.notNullValue;
 import static org.hamcrest.MatcherAssert.assertThat;
-
+// TODO: fix dependencies
+// the import org.hamcrest.core.SubstringMatcher.containsString cannot be resolved
+// import static org.hamcrest.core.SubstringMatcher.containsString;
+import static org.hamcrest.CoreMatchers.containsString;
 import java.util.Arrays;
 
 import org.json.JSONObject;
@@ -20,7 +23,6 @@ public class RuntimeTest extends BaseTest {
 	public void test1() {
 		try {
 			// Act
-
 			driver.navigate().to(URL);
 
 			final String selector = "input#searchInput";
@@ -45,7 +47,37 @@ public class RuntimeTest extends BaseTest {
 	}
 
 	@Test
-	public void test4() {
+	// invalid css selector
+	public void test2() {
+		try {
+			// Act
+			driver.navigate().to(URL);
+
+			final String selector = "input//searchInput";
+			CDPClient.setDebug(true);
+			CDPClient.sendMessage(MessageBuilder.buildCustomRuntimeEvaluateMessage(id,
+					selector, false));
+			// Assert
+			CDPClient.setDebug(false);
+			responseMessage = new JSONObject(CDPClient.getResponseMessage(id, null));
+			assertThat(responseMessage, notNullValue());
+			assertThat(responseMessage.has("result"), is(true));
+			result = responseMessage.getJSONObject("result");
+			System.err.println("getRuntimeEvaluateTest result: " + result);
+			for (String field : Arrays
+					.asList(new String[] { "type", "className", "description" })) {
+				assertThat(result.has(field), is(true));
+			}
+			assertThat(result.getString("className"), is("DOMException"));
+			assertThat(result.getString("description"),
+					containsString("is not a valid selector"));
+		} catch (Exception e) {
+			System.err.println("Web Driver exception (ignored): " + e.getMessage());
+		}
+	}
+
+	@Test
+	public void test3() {
 		try {
 			// Act
 
@@ -67,6 +99,38 @@ public class RuntimeTest extends BaseTest {
 				assertThat(result.has(field), is(true));
 			}
 			assertThat(result.getString("className"), is("XPathResult"));
+		} catch (Exception e) {
+			System.err.println("Web Driver exception (ignored): " + e.getMessage());
+		}
+	}
+
+	@Test
+	// invalid xpath
+	public void test4() {
+		try {
+			// Act
+
+			driver.navigate().to(URL);
+
+			final String selector = "//input[[]]";
+			CDPClient.setDebug(true);
+			CDPClient.sendMessage(MessageBuilder.buildCustomRuntimeEvaluateMessage(id,
+					selector, false));
+			// Assert
+			CDPClient.setDebug(false);
+			responseMessage = new JSONObject(CDPClient.getResponseMessage(id, null));
+			assertThat(responseMessage, notNullValue());
+			assertThat(responseMessage.has("result"), is(true));
+			result = responseMessage.getJSONObject("result");
+			System.err.println("getRuntimeEvaluateTest result: " + result);
+			for (String field : Arrays
+					.asList(new String[] { "type", "className", "description" })) {
+				assertThat(result.has(field), is(true));
+			}
+			assertThat(result.getString("className"), is("DOMException"));
+			assertThat(result.getString("description"),
+					containsString("is not a valid XPath expression"));
+
 		} catch (Exception e) {
 			System.err.println("Web Driver exception (ignored): " + e.getMessage());
 		}
