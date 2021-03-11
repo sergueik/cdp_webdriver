044b960c2bba550a018fb197027b923a7837fa3c
Serguei Kouzmine <kouzmine_serguei@yahoo.com>
2021-03-11 02:23:18 -0500

    intermediate revision -  somewhat simplified ShadowRootTest

--- src/test/java/example/ShadowRootDomTest.java
+++ src/test/java/example/ShadowRootDomTest.java
@@ -20,9 +20,6 @@ public class ShadowRootDomTest extends BaseTest {
 	private JSONObject result1 = null;
 	private JSONObject result2 = null;
 	private JSONObject result3 = null;
-	private JSONArray results1 = null;
-	private JSONArray results2 = null;
-	private JSONArray results3 = null;
 	Iterator<Object> results1Iterator = null;
 	Iterator<Object> results2Iterator = null;
 	Iterator<Object> results3Iterator = null;
@@ -41,18 +38,15 @@ public class ShadowRootDomTest extends BaseTest {
 			// Assert
 			result = new JSONObject(CDPClient.getResponseMessage(id, "root"));
 			System.err.println("DOM.getDocument object: " + result.toString(2));
-			results1 = result.getJSONArray("children");
-			results1Iterator = results1.iterator();
+			results1Iterator = result.getJSONArray("children").iterator();
 			while (results1Iterator.hasNext()) {
 				result1 = (JSONObject) results1Iterator.next();
 				if (result1.getString("nodeName").equals("HTML")) {
-					results2 = result1.getJSONArray("children");
-					results2Iterator = results2.iterator();
+					results2Iterator = result1.getJSONArray("children").iterator();
 					while (results2Iterator.hasNext()) {
 						result2 = (JSONObject) results2Iterator.next();
 						if (result2.getString("nodeName").equals("BODY")) {
-							results3 = result2.getJSONArray("children");
-							results3Iterator = results3.iterator();
+							results3Iterator = result2.getJSONArray("children").iterator();
 							while (results3Iterator.hasNext()) {
 								result3 = (JSONObject) results3Iterator.next();
 								if (result3.getString("nodeName").equals("HISTORY-APP")) {
@@ -65,7 +59,7 @@ public class ShadowRootDomTest extends BaseTest {
 					}
 				}
 			}
-			// NOTE :  another element with "shadowRoots" children: "HISTORY-ROUTER"
+			// NOTE : another element with "shadowRoots" children: "HISTORY-ROUTER"
 		} catch (IOException | WebSocketException | InterruptedException
 				| MessageTimeOutException e) {
 			System.err.println("Exception (ignored): " + e.toString());
